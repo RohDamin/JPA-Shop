b56b154655545a64da039c5021f050eb75c9e261
RohDamin <57509627+RohDamin@users.noreply.github.com>
2024-04-02 22:16:04 +0900

    feat: 상품 도메인 개발
    - 아이템 리포지토리 추가
    - 아이템 서비스 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,26 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories;
+
+    //==비즈니스 로직==//
+    // 데이터를 가지고 있는 쪽에 비즈니스 로직이 있는 게 관리하기 좋음
+    // 바깥에서 setter로 변경X, 비지니스 메서드로 변경
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.StockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.StockQuantity -= quantity;
+        if (restStock<0) { // 재고가 0보다 작으면 예외 발생
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.StockQuantity = restStock;
+    }
 }
