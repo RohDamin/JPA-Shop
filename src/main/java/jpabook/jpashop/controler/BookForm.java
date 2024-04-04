fbca218e272856fe54d7aa67edd1925353f3e84a
RohDamin <57509627+RohDamin@users.noreply.github.com>
2024-04-04 13:41:14 +0900

    feat: 상품 등록, 상품 목록 조회

---
+++ src/main/java/jpabook/jpashop/controler/BookForm.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.controler;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookForm {
+
+    // Item 공통속성
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    // 책과 관련된 속성
+    private String author;
+    private String isbn;
+
+}
---
+++ src/main/java/jpabook/jpashop/controler/ItemController.java
@@ -0,0 +1,74 @@
+package jpabook.jpashop.controler;
+//
+//import jpabook.jpashop.domain.item.Book;
+//import jpabook.jpashop.service.ItemService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class ItemController {
+//
+//    private final ItemService itemService;
+//
+//    @GetMapping("/items/new")
+//    public String createForm (Model model) {
+//        model.addAttribute("form", new BookForm());
+//        return "items/createItemForm";
+//    }
+//
+//    @PostMapping("/items/new")
+//    public String create(BookForm form) {
+//
+//        // set 사용하는 것보다 생성 메서드를 만드는 게 더 좋은 설계
+//        Book book = new Book();
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//
+//        itemService.saveItem(book);
+//        return "redirect:/items";
+//    }
+//}
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+    @PostMapping(value = "/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping(value = "/items")
+    public String list (Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
