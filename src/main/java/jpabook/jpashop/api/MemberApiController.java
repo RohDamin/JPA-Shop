8727007b1697a449df2d2c932eec7e30de2c9550
RohDamin <57509627+RohDamin@users.noreply.github.com>
2024-04-20 19:54:16 +0900

    refactor: 회원 등록 API v2
    - 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받음

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.api;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.Data;
@@ -15,12 +16,29 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    @PostMapping("api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+    @PostMapping("api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받음
+     * 장점:
+     *  - 엔티티와 프로젠테이션 계층을 위한 로직을 분리할 수 있음
+     *  - 엔티티와 API 스펙을 명확하게 분리할 수 있음
+     *  - 엔티티가 변해도 API 스펙이 변하지 않음
+     *  - 실무에서는 엔티티를 절대 외부에 노춣하면 안 됨!
+     */
+    @Data
+    static class CreateMemberRequest { // DTO
+        @NotEmpty
+        private String name;
+
+    }
 
     @Data
     static class CreateMemberResponse {
