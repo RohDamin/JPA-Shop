cdb31964075b4a382da5786b50c4e14611a5bf17
RohDamin <57509627+RohDamin@users.noreply.github.com>
2024-04-20 19:41:04 +0900

    refactor: 회원 등록 API v1
    - 요청 값으로 Member 엔티티를 직접 받음

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +17,17 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    /**
+     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받음
+     * 문제점:
+     * - 엔티티에 프로젠테이션 계층을 위한 로직이 추가됨
+     *   - 엔티티에 API 검증을 위한 로직이 들어감 (@NotEmpty 등)
+     *   - 어떤 API에서는 NotEmpty가 필요하지만, 어떤 API에서는 그렇지 않을 수도 있음 -> 모든 요구사항을 엔티티에 담기 힘듦
+     *   - 엔티티가 변경되면 API 스펙이 변함
+     * 해결방법:
+     * - API 요청 스펙에 맞춰 별도의 DTO를 파라미터로 받아야 함
+     */
+    @NotEmpty // 이름 필수값 -> 비어있으면 안됨
     private String name;
 
     @Embedded
